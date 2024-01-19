08e3205721f1b75bc1daf603afd741fa04ba37c8
GonzoTheDev <c20703429@mytudublin.ie>
2024-01-19 23:54:01 +0000

    linux update

--- Rotor-Cuda/Main.cpp
+++ Rotor-Cuda/Main.cpp
@@ -197,48 +197,7 @@ int main(int argc, char** argv)
 	// Global Init
 	Timer::Init();
 	rseed(Timer::getSeed32());
-	struct console
-	{
-		console(unsigned width, unsigned height)
-		{
-			SMALL_RECT r;
-			COORD      c;
-			hConOut = GetStdHandle(STD_OUTPUT_HANDLE);
-			if (!GetConsoleScreenBufferInfo(hConOut, &csbi))
-				throw runtime_error("  You must be attached to a human.");
 
-			r.Left =
-				r.Top = 0;
-			r.Right = width - 1;
-			r.Bottom = height - 1;
-			SetConsoleWindowInfo(hConOut, TRUE, &r);
-
-			c.X = width;
-			c.Y = height;
-			SetConsoleScreenBufferSize(hConOut, c);
-		}
-
-		~console()
-		{
-			SetConsoleTextAttribute(hConOut, csbi.wAttributes);
-			SetConsoleScreenBufferSize(hConOut, csbi.dwSize);
-			SetConsoleWindowInfo(hConOut, TRUE, &csbi.srWindow);
-		}
-
-		void color(WORD color = 0x07)
-		{
-			SetConsoleTextAttribute(hConOut, color);
-		}
-
-		HANDLE                     hConOut;
-		CONSOLE_SCREEN_BUFFER_INFO csbi;
-	};
-
-	//----------------------------------------------------------------------------
-	console con(220, 1000);
-	int color = 14;
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, color);
 	bool gpuEnable = false;
 	bool gpuAutoGrid = true;
 	int compMode = SEARCH_COMPRESSED;
